ffd5258d9e90155077124e8f923952ca23ebb884
majjeed <abdulmajeed.hersi@gmail.com>
2024-06-06 12:10:19 +0100

    complete solution without Extra Credit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import { LinkedList } from "./linked-list.js";
 import { Node } from "./node.js";
 
 let Data = new LinkedList();
-let A = new Node("a");
-let B = new Node("b");
-let C = new Node("c");
-let D = new Node("d");
+// let A = new Node("a");
+// let B = new Node("b");
+// let C = new Node("c");
+// let D = new Node("d");
+// let Z = new Node("z");
 
-Data.append(A);
-Data.append(B);
-Data.append(C);
-Data.append(D);
+Data.append("A");
+Data.append("B");
+Data.append("C");
+Data.append("D");
+Data.prepend("ZB");
 
-console.log(Data.size());
+//console.log(Data.size());
 console.log(Data);
+
+//console.log(Data.pop());
+//console.log(Data);
+
+//console.log(Data.at(4));
+
+//console.log(Data.contains("D"));
+
+//console.log(Data.find("D"));
+
+console.log(Data.toString());
--- src/linked-list.js
+++ src/linked-list.js
@@ -5,39 +5,114 @@ export class LinkedList {
     //this.node = new Node(node.value, node.nextNode);
     this.head = null;
     this.tail = null;
+    this.length = 0;
   }
 
   append(value) {
+    const newNode = new Node(value);
     if (this.head === null) {
-      this.head = new Node(value, null);
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      let tempTail = this.tail;
-      this.tail = new Node(value, tempTail);
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
     }
+    this.length++;
   }
 
   prepend(value) {
-    let tempHead = this.head;
-    this.head = new Node(value, tempHead);
+    const newNode = new Node(value);
+    newNode.nextNode = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   size() {
-    let count = 0;
-    let tempHead = this.head;
-    if (tempHead) {
-      while (tempHead.nextNode) {
-        count++;
-        tempHead = tempHead.nextNode;
-      }
-    }
-    return count;
+    return this.length;
   }
 
-  head() {}
-  tail() {}
-  at(index) {}
-  pop() {}
-  contains(value) {}
-  find(value) {}
-  toString() {}
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  at(index) {
+    let count = 1;
+    let currNode = this.head;
+
+    while (count <= index) {
+      if (count === index) {
+        return currNode;
+      } else {
+        currNode = currNode.nextNode;
+        count++;
+      }
+    }
+
+    return `Not Found at ${index}`;
+  }
+
+  pop() {
+    let currNode = this.head;
+    let lastNode = this.tail;
+
+    if (this.head === null) {
+      return null;
+    } else if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      while (currNode != this.tail) {
+        //for the second last node node.next.next would be null
+        //turn that into the tail...
+        if (currNode.nextNode.nextNode === null) {
+          this.tail = currNode;
+          this.tail.nextNode = null;
+        } else {
+          currNode = currNode.nextNode;
+        }
+      }
+    }
+
+    this.length--;
+    return lastNode;
+  }
+
+  contains(value) {
+    let currNode = this.head;
+    while (currNode != null) {
+      if (currNode.value === value) return true;
+      currNode = currNode.nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    let currNode = this.head;
+    let index = 1;
+    while (currNode !== null) {
+      if (currNode.value === value) {
+        return index;
+      } else {
+        currNode = currNode.nextNode;
+      }
+      index++;
+    }
+    return null;
+  }
+
+  toString() {
+    if (this.head === null) return "null";
+    let currNode = this.head;
+    let returnString = "";
+    while (currNode !== null) {
+      returnString = returnString + currNode.value + " -> ";
+      currNode = currNode.nextNode;
+    }
+
+    return returnString + "null";
+  }
 }
